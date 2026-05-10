9d78738681253932f0e0877a523687ff6b22fd1a
Sparyn <xbai-hang@qq.com>
2026-05-10 18:04:41 +0800

    Use target variables during attack evaluation

--- dataset.py
+++ dataset.py
@@ -1,9 +1,6 @@
 import torch
 import numpy as np
-import torch.nn as nn
-import torch.nn.functional as F
-from torch.utils.data import Dataset, DataLoader
-import tqdm
+from torch.utils.data import Dataset
 from timefeatures import time_features
 import pandas as pd
 
@@ -12,7 +9,6 @@ def load_raw_data(dataset_config):
     if 'PEMS' in dataset_config.dataset_name:
         raw_data = np.load(dataset_config.data_filename)['data']
         train_data_seq = raw_data[:int(0.6 * raw_data.shape[0])]
-        val_data_seq = raw_data[int(0.6 * raw_data.shape[0]):int(0.8 * raw_data.shape[0])]
         test_data_seq = raw_data[int(0.8 * raw_data.shape[0]):]
 
         train_mean = np.mean(train_data_seq, axis=(0, 1))
@@ -29,14 +25,10 @@ def load_raw_data(dataset_config):
         raw_data_stamps = raw_data.values[:, 0]
         raw_data_stamps = pd.to_datetime(raw_data_stamps)
 
-        # raw_data_stamps = raw_data_stamps.to_numpy()
-
         train_data_seq = raw_data_feats[:int(0.6 * raw_data_feats.shape[0])]
-        val_data_seq = raw_data_feats[int(0.6 * raw_data_feats.shape[0]):int(0.8 * raw_data_feats.shape[0])]
         test_data_seq = raw_data_feats[int(0.8 * raw_data_feats.shape[0]):]
 
         train_data_stamps = raw_data_stamps[:int(0.6 * raw_data_stamps.shape[0])]
-        val_data_stamps = raw_data_stamps[int(0.6 * raw_data_stamps.shape[0]):int(0.8 * raw_data_stamps.shape[0])]
         test_data_stamps = raw_data_stamps[int(0.8 * raw_data_stamps.shape[0]):]
 
         train_mean = np.mean(train_data_seq, axis=(0, 1))
@@ -53,15 +45,6 @@ def load_raw_data(dataset_config):
 
 class TimeDataset(Dataset):
     def __init__(self, raw_data, mean, std, device, num_for_hist=12, num_for_futr=12, timestamps=None):
-        # todo: use config to replace all the parameters
-        """
-        :param raw_data: for input, the raw data, shape (T, n, c), T: time span, n: sensor number, c: channels number
-            for storage, the target pattern, shape (n, c, T)
-        :param mean: the mean value of the raw data
-        :param std: the standard deviation of the raw data
-        :data: the clean data
-        :poisoned_data: the poisoned data for attack
-        """
         self.device = device
         self.data = raw_data
         self.use_timestamp = timestamps is not None
@@ -72,7 +55,6 @@ class TimeDataset(Dataset):
         else:
             self.timestamps = None
 
-        # permutate the data to (n, c, T)
         if len(self.data.shape) == 2:
             self.data = self.data.reshape(self.data.shape[0], self.data.shape[1], 1)
         self.data = np.transpose(self.data, (1, 2, 0)).astype(np.float32)
@@ -91,15 +73,10 @@ class TimeDataset(Dataset):
         return self.data.shape[-1] - self.num_for_hist - self.num_for_futr + 1
 
     def __getitem__(self, idx):
-        """
-        :param idx: the index of the data
-        :return:
-        """
         data = self.poisoned_data[:, 0:1, idx:idx + self.num_for_hist]
         data = self.normalize(data)
 
         poisoned_target = self.poisoned_data[:, 0, idx + self.num_for_hist:idx + self.num_for_hist + self.num_for_futr]
-
         clean_target = self.data[:, 0, idx + self.num_for_hist:idx + self.num_for_hist + self.num_for_futr]
         if not self.use_timestamp:
             return data, poisoned_target, clean_target, idx
@@ -124,10 +101,6 @@ class AttackEvaluateSet(TimeDataset):
         self.attacker = attacker
 
     def collate_fn(self, data):
-        """
-        :param data: the input data
-        :return: the attacked data by the attacker
-        """
         if self.use_timestamp:
             features, target, clean_target, input_stamps, target_stamps, idx = zip(*data)
             input_stamps = torch.stack(input_stamps, dim=0)
@@ -147,8 +120,9 @@ class AttackEvaluateSet(TimeDataset):
         features[:, self.attacker.atk_vars, :, -self.attacker.trigger_len:] = triggers
 
         target = clean_target.clone().detach().to(self.device)
-        target[:, self.attacker.atk_vars, :self.attacker.pattern_len] = \
-            self.attacker.target_pattern + features[:, self.attacker.atk_vars, :, -self.attacker.trigger_len - 1]
+        target_pattern = self.attacker._target_pattern_for(len(self.attacker.target_vars))
+        target[:, self.attacker.target_vars, :self.attacker.pattern_len] = \
+            target_pattern + features[:, self.attacker.target_vars, :, -self.attacker.trigger_len - 1]
 
         features = self.normalize(features)
         if not self.use_timestamp:
